ad5c2cf0039ff920bf43fb68ef3e14d202bfdc0b
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-05-19 18:12:13 -0400

    hate front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,117 +1,34 @@
-import logo from './logo.svg';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 import './App.css';
 
-function App() {
 
-  const openMenu = () => {
-    document.querySelector(".sidebar").classList.add("open");
-  }
-  const closeMenu = () =>{
-    document.querySelector(".sidebar").classList.remove("open");
-  }
+import data from './data';
+
+function App() {
   return (
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}>
-            &#9776;
-          </button>
-          <a href="index.html">ScrollnShop</a>
-        </div>
-        <div className="header-links">
-          <a href="cart.html">Cart</a>
-          <a href="signin.html">Sign In</a>
-        </div>
+    <div>
+      <header>
+        <a href='/'>Scroll n Shop</a>
       </header>
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
-        <ul>
-          <li>
-            <a href="index.html">Pants</a>
-          </li>
-  
-          <li>
-            <a href="index.html">Shirts</a>
-          </li>
-  
-        </ul>
-      </aside>
-      <main className="main">
-        <div className="content">
-          <ul className="products">
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-            <li>
-              <div className="product">
-                <img className="product-image" src="/images/shirts1.jpg" alt="product" />
-                <div className="product-name">
-                  <a href="product.html">3 pack Short Sleeves</a>
-                </div>
-                <div className="product-brand">Nike</div>
-                <div className="product-price">$60</div>
-                <div className="product-rating">4.5 Stars (10 Reviews)</div>
-              </div>
-            </li>
-          </ul>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => {
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info"></div>
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p><strong>${product.price}</strong></p>
+              <button>Add to cart</button>
+            </div>
+          })}
         </div>
       </main>
-      <footer className="footer">
-        All right reserved.
-      </footer>
     </div>
   );
 }
---
+++ frontend/src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+      {
+      name: 'Nike short sleeve',
+      slug: 'nike-short-sleeve',
+      category: 'Shirts',
+      image: '/images/ss1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+      },
+      {
+        name: 'Nike Button shirt',
+        slug: 'nike-button-shirt',
+        category: 'Shirts',
+        image: '/images/ls1.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      },
+      {
+        name: 'Nike Best slacks',
+        slug: 'nike-best-slacks',
+        category: 'Pants',
+        image: '/images/pt1.jpg',
+        price: 120,
+        countInStock: 10,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        description: 'high quality shirt',
+      },
+    ]
+  }
+
+  export default data;
