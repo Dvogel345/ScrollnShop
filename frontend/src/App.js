523c73b11e0eb6109c8e6d6f0d2e55a340fe3cc8
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-05-20 18:07:39 -0400

    added boottrap to site, still needs work, stuck but proceedig with it

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import React from 'react';
-
+import Navbar from 'react-bootstrap/Container';
+import Container from 'react-bootstrap/Navbar';
+import { LinkContainer } from 'react-router-bootstrap';
 // import data from './data';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
@@ -8,16 +10,27 @@ import ProductScreen from './screens/ProductScreen';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to='/'>Scroll n Shop</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar className='brand'>Scroll n Shop</Navbar>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />}/>
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
