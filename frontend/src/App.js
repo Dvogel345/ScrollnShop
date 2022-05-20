6a5f7e3af178a1bb559f83875329db726d17bb0a
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-05-20 15:54:05 -0400

    added two routes for product and homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import React from 'react';
 
-import data from './data';
+// import data from './data';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <BrowserRouter>
       <div>
         <header>
-          <a href='/'>Scroll n Shop</a>
+          <Link to='/'>Scroll n Shop</Link>
         </header>
         <main>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:slug" element={<ProductScreen />}/>
           </Routes>
-          <h1>Featured Products</h1>
-          <div className="products">
-            {data.products.map((product) => (
-              <div className="product" key={product.slug}>
-                <a href={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </a>
-                <div className="product-info"></div>
-                <a href={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p><strong>${product.price}</strong></p>
-                <button>Add to cart</button>
-              </div>
-            ))}
-          </div>
         </main>
       </div>
     </BrowserRouter>
