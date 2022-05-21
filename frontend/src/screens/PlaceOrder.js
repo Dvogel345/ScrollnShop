19f2d54f1b8cd9cc13b0a859fba9a26dce01439f
Derek Vogel <78238679+Dvogel345@users.noreply.github.com>
2022-05-21 15:31:45 -0400

    implemented orderModel, orderRoute, and auth to check if token matches order from a speficied user req

--- frontend/src/screens/PlaceOrder.js
+++ frontend/src/screens/PlaceOrder.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useReducer } from 'react';
 import Col from 'react-bootstrap/esm/Col';
 import Row from 'react-bootstrap/esm/Row';
 import Card from 'react-bootstrap/Card';
@@ -8,16 +8,38 @@ import CheckoutSteps from '../components/CheckoutSteps';
 import { Link, useNavigate } from 'react-router-dom';
 import { Store } from '../Store';
 import Button from 'react-bootstrap/esm/Button';
+import { toast } from 'react-toastify';
+import { getError } from '../utils';
+import axios from 'axios';
+import LoadingBox from '../components/LoadingBox';
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'CREATE_REQUEST':
+            return { ...state, loading: true };
+        case 'CREATE_SUCCESS':
+            return { ...state, loading: false };
+        case 'CREATE_FAIL':
+            return { ...state, loading: false };
+        default:
+            return state;
+    }
+}
 
 export default function PlaceOrder() {
     const navigate = useNavigate();
+
+    const [{ loading }, dispatch] = useReducer(reducer, {
+        loading: false,
+    });
+
     const { state, dispatch: ctxDispatch } = useContext(Store);
     const { cart, userInfo } = state;
 
     // rounds a number 123.125 to two decimal points such as 123.13
-    const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100; 
+    const round2 = (num) => Math.round(num * 100 + Number.EPSILON) / 100;
     cart.itemsPrice = round2(
-        cart.cartItems.reduce((a,c)=> a + c.quantity * c.price, 0)
+        cart.cartItems.reduce((a, c) => a + c.quantity * c.price, 0)
     );
     // if shippingPrice is > 100 make it 0 otherwise make it 10
     cart.shippingPrice = cart.itemsPrice > 100 ? round2(0) : round2(10);
@@ -25,12 +47,42 @@ export default function PlaceOrder() {
     cart.taxPrice = round2(0.15 * cart.itemsPrice);
     cart.totalPrice = cart.itemsPrice + cart.shippingPrice + cart.taxPrice;
 
-    const placeOrderHandler = async () => {};
-    useEffect(() =>{
-        if(!cart.paymentMethod) {
+    const placeOrderHandler = async () => {
+        try {
+          dispatch({ type: 'CREATE_REQUEST' });
+            // used to authenticate user thats logged in vs someone not logged in trying to commit fraud
+          const { data } = await axios.post(
+            '/api/orders',
+            {
+              orderItems: cart.cartItems,
+              shippingAddress: cart.shippingAddress,
+              paymentMethod: cart.paymentMethod,
+              itemsPrice: cart.itemsPrice,
+              shippingPrice: cart.shippingPrice,
+              taxPrice: cart.taxPrice,
+              totalPrice: cart.totalPrice,
+            },
+            {
+              headers: {
+                authorization: `Bearer ${userInfo.token}`,
+              },
+            }
+          );
+          ctxDispatch({ type: 'CART_CLEAR' });
+          dispatch({ type: 'CREATE_SUCCESS' });
+          localStorage.removeItem('cartItems');
+          navigate(`/order/${data.order._id}`);
+        } catch (err) {
+          dispatch({ type: 'CREATE_FAIL' });
+          toast.error(getError(err));
+        }
+      };
+
+    useEffect(() => {
+        if (!cart.paymentMethod) {
             navigate('/payment')
         }
-    },[cart, navigate]);
+    }, [cart, navigate]);
 
     return <div>
         <CheckoutSteps step1 step2 step3 step4></CheckoutSteps>
@@ -80,53 +132,54 @@ export default function PlaceOrder() {
                 </Card>
             </Col>
             <Col md={4}>
-          <Card>
-            <Card.Body>
-              <Card.Title>Order Summary</Card.Title>
-              <ListGroup variant="flush">
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Items</Col>
-                    <Col>${cart.itemsPrice.toFixed(2)}</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Shipping</Col>
-                    <Col>${cart.shippingPrice.toFixed(2)}</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>Tax</Col>
-                    <Col>${cart.taxPrice.toFixed(2)}</Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <Row>
-                    <Col>
-                      <strong> Order Total</strong>
-                    </Col>
-                    <Col>
-                      <strong>${cart.totalPrice.toFixed(2)}</strong>
-                    </Col>
-                  </Row>
-                </ListGroup.Item>
-                <ListGroup.Item>
-                  <div className="d-grid">
-                    <Button
-                      type="button"
-                      onClick={placeOrderHandler}
-                      disabled={cart.cartItems.length === 0}
-                    >
-                      Place Order
-                    </Button>
-                  </div>
-                </ListGroup.Item>
-              </ListGroup>
-            </Card.Body>
-          </Card>
-        </Col>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Order Summary</Card.Title>
+                        <ListGroup variant="flush">
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Items</Col>
+                                    <Col>${cart.itemsPrice.toFixed(2)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Shipping</Col>
+                                    <Col>${cart.shippingPrice.toFixed(2)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>Tax</Col>
+                                    <Col>${cart.taxPrice.toFixed(2)}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <Row>
+                                    <Col>
+                                        <strong> Order Total</strong>
+                                    </Col>
+                                    <Col>
+                                        <strong>${cart.totalPrice.toFixed(2)}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroup.Item>
+                            <ListGroup.Item>
+                                <div className="d-grid">
+                                    <Button
+                                        type="button"
+                                        onClick={placeOrderHandler}
+                                        disabled={cart.cartItems.length === 0}
+                                    >
+                                        Place Order
+                                    </Button>
+                                </div>
+                                {loading && <LoadingBox></LoadingBox>}
+                            </ListGroup.Item>
+                        </ListGroup>
+                    </Card.Body>
+                </Card>
+            </Col>
         </Row>
     </div>
 }
